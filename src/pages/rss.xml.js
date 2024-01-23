28767bcddd79d24a685c7336783dccb4752b22d0
Gigi <47110839+DarthGigi@users.noreply.github.com>
2024-01-23 18:24:22 +0100

    Update heroImage paths in newsroom introduction and rss.xml.js

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -26,7 +26,7 @@ export async function get() {
         <lastBuildDate>${new Date().toUTCString()}</lastBuildDate>
         <ttl>60</ttl>
         <image>
-            <url>https://newsroom.minionah.com/images/newsroom.png</url>
+            <url>https://newsroom.minionah.com/images/ogBanner.png</url>
             <title>${SITE_TITLE}</title>
             <link>${SITE_URL}</link>
         </image>
