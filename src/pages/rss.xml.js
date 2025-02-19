6c7d2488f02bee8c5fa65c5f87e4bd416322c4ea
DarthGigi <47110839+DarthGigi@users.noreply.github.com>
2025-02-19 05:32:30 +0100

    refactor: reorder imports in PrivacyTos.astro and rss.xml.js for consistency

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,6 +1,6 @@
 import rss from "@astrojs/rss";
 import { getCollection } from "astro:content";
-import { SITE_TITLE, SITE_DESCRIPTION, SITE_URL } from "../consts";
+import { SITE_DESCRIPTION, SITE_TITLE, SITE_URL } from "../consts";
 
 export async function get() {
   const posts = await getCollection("blog");
