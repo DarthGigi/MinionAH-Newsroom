bf846c8a20daedc25af2d6dbba24264968b6e9d1
DarthGigi <47110839+DarthGigi@users.noreply.github.com>
2025-07-12 23:39:01 +0200

    chore: update Svelte adapter from static to Vercel for deployment

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from "@sveltejs/adapter-static";
+import adapter from "@sveltejs/adapter-vercel";
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 import { mdsx } from "mdsx";
 import { mdsxConfig } from "./mdsx.config.js";
